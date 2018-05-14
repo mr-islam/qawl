f7f54cdc2e6b5ae79185e0b46d90dbf5a8a240cb
Naved Islam <navedcoded@gmail.com>
2018-05-14 16:17:12 +0500

    starter up
    split the things, clear vision of app, extremely minimal basic implementation right now,
    but easy sailing from here, and only premium things might be harder. get MVP out soon

---
+++ script.js
@@ -0,0 +1,12 @@
+	var leftPageNumber = 4;
+	var rightPageNumber = 3;
+
+	document.getElementById("leftPage").src = "../quran/hi/png-d150/"+leftPageNumber+".png"; 
+	document.getElementById("rightPage").src = "../quran/hi/png-d150/"+rightPageNumber+".png"; 
+
+	/*
+	event buttonLeft press, right press, add +2
+	animation to fade away/flip/slideRight, then block:none
+	change src of image left/right
+	block:display again to show image <--- how would an animation fit here?
+	*/
