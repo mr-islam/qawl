6327fdd63c26bdb9841b259869dbf6f575fee04b
Naved Islam <navedcoded@gmail.com>
2018-05-17 16:25:14 +0500

    added barely working animation for pageflip
    it only works the first time :(… so something wrong with classes

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 var rightPageNumber = 3;
-var leftPageNumber = rightPageNumber + 1;
+var leftPageNumber = rightPageNumber + 1; //or, localStorage, later.
 
-function setPage() {
-	document.getElementById("leftPage").src = "../quran/hi/png-d150/"+leftPageNumber+".png"; 
-	document.getElementById("rightPage").src = "../quran/hi/png-d150/"+rightPageNumber+".png";
+var leftPage = document.getElementById("leftPage");
+var rightPage = document.getElementById("rightPage");
+
+/*var element = document.getElementById("div1");
+element.classList.add("otherclass");*/
+
+function renderPage() {
+	leftPage.src = "../quran/hi/png-d150/"+leftPageNumber+".png"; 
+	rightPage.src = "../quran/hi/png-d150/"+rightPageNumber+".png";
 }
+
 
 function changePage(increment) {
+	console.log('start change');
+	leftPage.classList.remove("pageEnter");
+	rightPage.classList.remove("pageEnter");
+	leftPage.classList.add("pageExit");
+	rightPage.classList.add("pageExit");
+
+	console.log('change 2');
 	rightPageNumber += increment;
-	leftPageNumber = rightPageNumber + 1
-	setPage();
+	leftPageNumber = rightPageNumber + 1;
+	console.log(rightPageNumber);
+
+	console.log('change 3');
+	leftPage.classList.remove("pageExit");
+	rightPage.classList.remove("pageExit");
+	leftPage.classList.add("pageEnter");
+	rightPage.classList.add("pageEnter");
+	console.log('change 4');
+
+	renderPage();
 }
 
-setPage();
+renderPage();
+console.log('renderPage');
 /*
 page change logic flow:
 	event buttonLeft press, right press, add +2
