78bb883835105d6b2fc91f2e83a527f3fda5361c
Naved Islam <navedcoded@gmail.com>
2018-05-17 15:46:53 +0500

    fixed page turning

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-	var leftPageNumber = 4;
-	var rightPageNumber = 3;
+var rightPageNumber = 3;
+var leftPageNumber = rightPageNumber + 1;
 
+function setPage() {
 	document.getElementById("leftPage").src = "../quran/hi/png-d150/"+leftPageNumber+".png"; 
-	document.getElementById("rightPage").src = "../quran/hi/png-d150/"+rightPageNumber+".png"; 
+	document.getElementById("rightPage").src = "../quran/hi/png-d150/"+rightPageNumber+".png";
+}
 
-	/*
+function changePage(increment) {
+	rightPageNumber += increment;
+	leftPageNumber = rightPageNumber + 1
+	setPage();
+}
+
+setPage();
+/*
+page change logic flow:
 	event buttonLeft press, right press, add +2
 	animation to fade away/flip/slideRight, then block:none
 	change src of image left/right
 	block:display again to show image <--- how would an animation fit here?
-	*/
+*/
