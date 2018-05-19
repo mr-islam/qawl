4ce2de1139d65f814b632d2ec3188a290a4b6a9a
Naved Islam <navedcoded@gmail.com>
2018-05-19 19:43:01 +0500

    fixed flipping page beyond limits

--- script.js
+++ script.js
@@ -12,27 +12,32 @@ function renderPage() {
 	rightPage.src = "../quran/hi/png-d150/"+rightPageNumber+".png";
 }
 
-
 function changePage(increment) {
-	console.log('start change');
-	leftPage.classList.remove("pageEnter");
-	rightPage.classList.remove("pageEnter");
-	leftPage.classList.add("pageExit");
-	rightPage.classList.add("pageExit");
+	
 
-	console.log('change 2');
-	rightPageNumber += increment;
-	leftPageNumber = rightPageNumber + 1;
-	console.log(rightPageNumber);
 
-	console.log('change 3');
-	leftPage.classList.remove("pageExit");
-	rightPage.classList.remove("pageExit");
-	leftPage.classList.add("pageEnter");
-	rightPage.classList.add("pageEnter");
-	console.log('change 4');
+	if ((rightPageNumber > 1) && (rightPageNumber < 604)) {
+		//so it wont go beyond the page limits
+		console.log('start change');
+		leftPage.classList.remove("pageEnter");
+		rightPage.classList.remove("pageEnter");
+		leftPage.classList.add("pageExit");
+		rightPage.classList.add("pageExit");
 
-	renderPage();
+		console.log('change 2');
+		rightPageNumber += increment;
+		leftPageNumber = rightPageNumber + 1;
+		console.log(rightPageNumber);
+
+		console.log('change 3');
+		leftPage.classList.remove("pageExit");
+		rightPage.classList.remove("pageExit");
+		leftPage.classList.add("pageEnter");
+		rightPage.classList.add("pageEnter");
+		console.log('change 4');
+
+		renderPage();
+	}
 }
 
 renderPage();
