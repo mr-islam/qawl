6c74135f6db20bde6a34d2db284ac9d42636b471
Naved Islam <navedcoded@gmail.com>
2018-06-03 16:45:26 +0500

    linting, chrome compat
    turnPage is not working… gotta figure dat

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var rightPageNumber = localStorage.getItem('rightPageNumberStored') || 3;
+var rightPageNumber = localStorage.getItem("rightPageNumberStored") || 3;
 var leftPageNumber = parseInt(rightPageNumber) + 1;
 
 var leftPageElement = document.getElementById("leftPage");
 var rightPageElement = document.getElementById("rightPage");
 
 var userPageInput = document.getElementById("pageNumberInput").value;
-var userPageInputInt = parseInt(userPageInput)
+var userPageInputInt = parseInt(userPageInput);
 
 function changePage() { // changes page to whatevers in input
 	if (userPageInputInt < 604 && userPageInputInt > -1) {
@@ -16,7 +16,7 @@ function changePage() { // changes page to whatevers in input
 			rightPageNumber = userPageInputInt;
 			leftPageNumber = parseInt(rightPageNumber) + 1;
 		}
-		localStorage.setItem('rightPageNumberStored', rightPageNumber);
+		localStorage.setItem("rightPageNumberStored", rightPageNumber);
 		updatePageView();
 		document.getElementById("pageNumberInput").value = JSON.stringify(userPageInputInt);
 	}
@@ -24,40 +24,43 @@ function changePage() { // changes page to whatevers in input
 function choicePage() {
 	var userPageInput = document.getElementById("pageNumberInput").value;
 	var userPageInputInt = parseInt(userPageInput);
-	if (userPageInputInt % 2 === 0) { // this and below is repeated changePage(); I messed up vars
+	if (userPageInputInt % 2 === 0) { // this and below is repeated changePage()…
 		leftPageNumber = userPageInputInt;
 		rightPageNumber = leftPageNumber - 1;
 	} else {
 		rightPageNumber = userPageInputInt;
 		leftPageNumber = parseInt(rightPageNumber) + 1;
 	}
-	localStorage.setItem('rightPageNumberStored', rightPageNumber);
+	localStorage.setItem("rightPageNumberStored", rightPageNumber);
 	updatePageView();
 }
 function turnPage(increment) {
+	console.log("turn page called")
 	userPageInputInt += increment;
 	changePage();
 }
 function selectSurah() { // working for even pages, not odd
-	var selectedSurah = parseInt(document.getElementById('surahSelect').value);
+	var selectedSurah = parseInt(document.getElementById("surahSelect").value);
 	userPageInputInt = selectedSurah;
 	console.log(selectedSurah);
-	console.log(userPageInputInt)
+	console.log(userPageInputInt);
 	changePage();
 }
 function changeZoom(increment) {
 	var currentZoom = parseInt(localStorage.getItem("currentZoomStored")) || 100;
 	currentZoom += increment;
-	console.log(currentZoom);
+	console.log("zoom="+currentZoom);
 	if (currentZoom < 100) {
-		document.getElementById("wrapper").style['max-width'] = currentZoom + "%";
+		document.body.style.width = 100 + "%";
+		document.getElementById("wrapper").style["max-width"] = currentZoom + "%";
 	} else if (currentZoom > 100) {
 		document.body.style.width = currentZoom + "%";
+		document.getElementById("wrapper").style["max-width"] = 100 + "%";
 	}
-	localStorage.setItem('currentZoomStored', currentZoom)
+	localStorage.setItem("currentZoomStored", currentZoom);
 }
 function updatePageView() {
-	console.log('page set: r='+ rightPageNumber + ' l='+ leftPageNumber);
+	console.log("page set: r="+ rightPageNumber + " l="+ leftPageNumber);
 	leftPage.src = "assets/mushaf-green/"+leftPageNumber+".png";
 	rightPage.src = "assets/mushaf-green/"+rightPageNumber+".png";
 }
@@ -65,10 +68,10 @@ function updatePageView() {
 updatePageView(); //resume reading from last place
 changeZoom(0); //to get last zoom set
 
-(function() { // fills in <select> from other .js file
-    var ele = document.getElementById('surahSelect');
-    for (var i = 0; i < surahs.length; i++) {
+(function() { // fills in <select> from surahs.js
+    var ele = document.getElementById("surahSelect");
+    for (let i = 0; i < surahs.length; i++) {
         ele.innerHTML = ele.innerHTML +
-            '<option value="' + surahs[i]['pageGreen'] + '">' + surahs[i]['name'] + '</option>';
+            '<option value="' + surahs[i]['pageGreen'] + '">' + surahs[i]['name'] + '</option>'; 
     }
 })();
