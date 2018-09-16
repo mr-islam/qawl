08ba78c1c0df496be16d0e0e9cca6098ec9883ea
Naved Islam <navedcoded@gmail.com>
2018-09-16 14:15:35 +0500

    fix menu bar windows

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    autoHideMenuBar: true
+  });
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
