016591afd84bedb0926242dd9cf904c235af8737
Naved Islam <navedcoded@gmail.com>
2018-09-25 18:26:49 +0500

    add window state manager

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const { ipcMain } = require('electron')
+const windowStateKeeper = require('electron-window-state')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+/*
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -15,8 +17,10 @@ function createWindow () {
     minWidth: 800,
     minHeight: 600,
     resizable: true,
-    scrollBounce: true
-  });
+    scrollBounce: true,
+    backgroundColor: '#073642',
+    icon: __dirname + '/assets/icon.png'
+});
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
   })
@@ -34,10 +38,51 @@ function createWindow () {
   })
 }
 
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+*/
+
+app.on('ready', function () {
+  // Load the previous state with fallback to defaults
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800
+  });
+  // Create the window using the state information
+  mainWindow = new BrowserWindow({
+    title: "Qawl — Desktop Quran Reader",
+    show: false,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    resizable: true,
+    scrollBounce: true,
+    backgroundColor: '#073642',
+    icon: __dirname + '/assets/icon.png'
+  });
+
+  mainWindowState.manage(mainWindow);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+  mainWindow.setMenu(null)
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
